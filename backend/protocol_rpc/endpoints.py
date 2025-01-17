3f6801acf6365bb6907098369199c8cb9300cd23
kstroobants <stroobants.kristof@hotmail.com>
2025-01-17 10:38:16 +0000

    refactor: change wrapper name

--- backend/protocol_rpc/endpoints.py
+++ backend/protocol_rpc/endpoints.py
@@ -56,7 +56,7 @@ import os
 
 
 ####### WRAPPER TO BLOCK ENDPOINTS FOR HOSTED ENVIRONMENT #######
-def check_vite_hosted(func):
+def check_forbidden_method_in_hosted_studio(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         if os.getenv("VITE_IS_HOSTED") == "true":
@@ -76,7 +76,7 @@ def ping() -> str:
 
 
 ####### SIMULATOR ENDPOINTS #######
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def clear_db_tables(session: Session, tables: list) -> None:
     for table_name in tables:
         table = Table(
@@ -101,7 +101,7 @@ def fund_account(
     return transaction_hash
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def reset_defaults_llm_providers(llm_provider_registry: LLMProviderRegistry) -> None:
     llm_provider_registry.reset_defaults()
 
@@ -112,7 +112,7 @@ async def get_providers_and_models(
     return await llm_provider_registry.get_all_dict()
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def add_provider(llm_provider_registry: LLMProviderRegistry, params: dict) -> int:
     provider = LLMProvider(
         provider=params["provider"],
@@ -127,7 +127,7 @@ def add_provider(llm_provider_registry: LLMProviderRegistry, params: dict) -> in
     return llm_provider_registry.add(provider)
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def update_provider(
     llm_provider_registry: LLMProviderRegistry, id: int, params: dict
 ) -> None:
@@ -143,12 +143,12 @@ def update_provider(
     llm_provider_registry.update(id, provider)
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def delete_provider(llm_provider_registry: LLMProviderRegistry, id: int) -> None:
     llm_provider_registry.delete(id)
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def create_validator(
     validators_registry: ValidatorsRegistry,
     accounts_manager: AccountsManager,
@@ -184,7 +184,7 @@ def create_validator(
     )
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 async def create_random_validator(
     validators_registry: ValidatorsRegistry,
     accounts_manager: AccountsManager,
@@ -203,7 +203,7 @@ async def create_random_validator(
     )[0]
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 async def create_random_validators(
     validators_registry: ValidatorsRegistry,
     accounts_manager: AccountsManager,
@@ -238,7 +238,7 @@ async def create_random_validators(
     return response
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def update_validator(
     validators_registry: ValidatorsRegistry,
     accounts_manager: AccountsManager,
@@ -279,7 +279,7 @@ def update_validator(
     return validators_registry.update_validator(validator)
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def delete_validator(
     validators_registry: ValidatorsRegistry,
     accounts_manager: AccountsManager,
@@ -293,7 +293,7 @@ def delete_validator(
     return validator_address
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def delete_all_validators(
     validators_registry: ValidatorsRegistry,
 ) -> list:
@@ -557,7 +557,7 @@ def set_transaction_appeal(
     transactions_processor.set_transaction_appeal(transaction_hash, True)
 
 
-@check_vite_hosted
+@check_forbidden_method_in_hosted_studio
 def set_finality_window_time(consensus: ConsensusAlgorithm, time: int) -> None:
     consensus.set_finality_window_time(time)
 
