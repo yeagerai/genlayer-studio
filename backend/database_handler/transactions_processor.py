79dba5d84172c66687f7e802c1ff054c1c70a543
kstroobants <stroobants.kristof@hotmail.com>
2025-01-22 12:51:56 +0000

    fix: check status typo

--- backend/database_handler/transactions_processor.py
+++ backend/database_handler/transactions_processor.py
@@ -350,8 +350,8 @@ class TransactionsProcessor:
         transaction = (
             self.session.query(Transactions).filter_by(hash=transaction_hash).one()
         )
-        if (transaction.status == TransactionStatus.ACCEPTED.value) or (
-            transaction.status == TransactionStatus.UNDETERMINED.value
+        if (transaction.status == TransactionStatus.ACCEPTED) or (
+            transaction.status == TransactionStatus.UNDETERMINED
         ):
             transaction.appealed = appeal
 
