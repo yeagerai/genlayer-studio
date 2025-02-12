4e2c8033e99b3e42755a5d73f7f785992e0ce808
kstroobants <130580298+kstroobants@users.noreply.github.com>
2025-02-12 13:21:30 +0000

    feat: consensus thread improvements (#812)
    * feat: add rollup transaction db table
    
    * fix: drop audit table, change id and nonce of rollup, only make one finalized rollup transaction
    
    * create rollup transaction for every validator
    
    * add function to mock in consensus test
    
    * feat: add appeal window, accepted queue, basic test
    
    * fix: old tests were stuck on accepted state, add usage of thread
    
    * refactor consensus into states
    
    * feat: added appeal flow when transaction is accepted including the loop when appeal failed and succeeded
    
    * fix: adding tests for appeals and fixing minor bugs
    
    * refactor: merge main and PR #573 into this branch
    
    * feat: add appeal_failed in db, select new validators when appealed based on formula
    
    * docs: cleanup consensus mechanism base file
    
    * test: checking the number of validators for different appeals
    
    * feat: adding appeal window to undetermined state
    
    * feat: change timestamp_accepted and add appeal_undetermined in database
    
    * feat: undetermined to pending, activate frontend button in undetermined state and add button also to modal
    
    * feat: leader only has no appeal button and no appeal window
    
    * feat: implement the state transitions to process the appeal
    - Add N+2 validators, remove leader
    - Use latest data of transaction when in pending state and not the old one from the crawler
    - Write consensus data before setting status to have it updated in frontend when going to transaction info modal
    - Do not deploy contract when transaction was in the undetermined state
    
    * test: add test for leader appeals
    
    * refactor: merge 593-appeals-add-validators-when-appealed into 604-appeals-implement-sequential-appeals-fail
    
    * refactor: merging changed file permissions
    
    * fix: appealing a write method gave a KeyError because of wrong conversation of transaction
    
    * docs: update transaction_processor argument description
    
    * fix: all appeals disagreed because of pending_transactions type
    
    * refactor: undo change because of merge
    
    * fix: do not reset finality window when leader appeal failed, leader_only check in appeal_window
    
    * fix: set value in database migration file
    
    * fix: add leader_only check for modal appeal button, comment out modal appeal button
    
    * fix: do not show appeal button when finality window is finished, checking on finalized state gave a small delay in showing when appeal failed and finality window was exceeded
    
    * fix: we do not emit messages when the transaction goes from undetermined to finalized
    
    * fix: complete merge
    
    * fix: redirect leader appeal is processed by appeal queue, not pending queue
    
    * fix: test increase wait time to get to finalized
    
    * fix: use appeal property of the transaction in frontend
    
    * fix: comment out appeal buttons
    
    * refactor: clean up prints
    
    * feat: appeal window parallel tasks, removed pending check because of queued transactions db field, updated consensus test
    
    * fix: check status typo
    
    * fix: update consensus test
    
    * fix: update test_get_highest_timestamp
    
    * fix: changed queued to activated status, changed documentation
    
    * docs: update function docs
    
    * fix: add session.commit() as it was part of removed queued function
    
    * fix: remove from_address when sorting by contract
    
    * fix: remove from_address also from test_helpers
    
    * fix: solve failed consensus test
    
    * fix: pre-commit fail
    
    ---------
    
    Co-authored-by: Cristiam Da Silva <cristiam86@gmail.com>

--- backend/database_handler/chain_snapshot.py
+++ backend/database_handler/chain_snapshot.py
@@ -2,6 +2,7 @@
 
 from typing import List
 from sqlalchemy.orm import Session
+from collections import defaultdict
 
 from backend.database_handler.transactions_processor import (
     TransactionStatus,
@@ -28,6 +29,7 @@ class ChainSnapshot:
         pending_transactions = (
             self.session.query(Transactions)
             .filter(Transactions.status == TransactionStatus.PENDING)
+            .order_by(Transactions.created_at)
             .all()
         )
         return [
@@ -43,22 +45,28 @@ class ChainSnapshot:
         """Return the list of all validators."""
         return self.all_validators
 
-    def _load_accepted_undetermined_transactions(self) -> List[dict]:
-        """Load and return the list of accepted and undetermined transactions from the database."""
+    def _load_accepted_undetermined_transactions(self) -> dict[str, List[dict]]:
+        """Load and return the list of accepted and undetermined transactions from the database,
+        grouped by address."""
 
-        accepted_transactions = (
+        accepted_undetermined_transactions = (
             self.session.query(Transactions)
             .filter(
                 (Transactions.status == TransactionStatus.ACCEPTED)
                 | (Transactions.status == TransactionStatus.UNDETERMINED)
             )
-            .order_by(Transactions.timestamp_awaiting_finalization)
+            .order_by(Transactions.created_at)
             .all()
         )
-        return [
-            TransactionsProcessor._parse_transaction_data(transaction)
-            for transaction in accepted_transactions
-        ]
+
+        # Group transactions by address
+        transactions_by_address = defaultdict(list)
+        for transaction in accepted_undetermined_transactions:
+            address = transaction.to_address
+            transactions_by_address[address].append(
+                TransactionsProcessor._parse_transaction_data(transaction)
+            )
+        return transactions_by_address
 
     def get_accepted_undetermined_transactions(self):
         """Return the list of accepted and undetermined transactions."""
