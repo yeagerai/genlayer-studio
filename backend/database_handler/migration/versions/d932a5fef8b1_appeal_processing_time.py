0b7ee83087e01a69d2d39ee7869166d8214bab87
kstroobants <stroobants.kristof@hotmail.com>
2025-02-18 11:40:17 +0000

    fix: database migration file revision duplicates

--- backend/database_handler/migration/versions/d932a5fef8b1_appeal_processing_time.py
+++ backend/database_handler/migration/versions/d932a5fef8b1_appeal_processing_time.py
@@ -1,7 +1,7 @@
 """appeal_processing_time
 
 Revision ID: d932a5fef8b1
-Revises: 67943badcbe9
+Revises: 2de9c3151194
 Create Date: 2025-01-31 15:39:44.618075
 
 """
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "d932a5fef8b1"
-down_revision: Union[str, None] = "67943badcbe9"
+down_revision: Union[str, None] = "2de9c3151194"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
