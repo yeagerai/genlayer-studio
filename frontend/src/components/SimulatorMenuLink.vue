e2692ef92195462af0efebde7732ac715bd3d8df
henry-akwerigbe <henadek.ha@gmail.com>
2025-03-10 14:08:00 +0100

    feat: added icon at top right and input field modal to import contract from address #895

--- frontend/src/components/SimulatorMenuLink.vue
+++ frontend/src/components/SimulatorMenuLink.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
 defineProps({
-  href: { type: String, default: '/' },
+  href: { type: String, default: '' },
 });
+
+const emit = defineEmits(['click']);
 </script>
 
 <template>
-  <a :href="href" target="_blank">
-    <div class="group p-3">
-      <div
-        class="h-4 w-4 fill-primary opacity-50 group-hover:opacity-70 dark:fill-white"
-      >
-        <slot />
-      </div>
+  <component
+    :is="href ? 'a' : 'button'"
+    :href="href || undefined"
+    target="_blank"
+    @click="
+      emit('click');
+      console.log('SimulatorMenuLink clicked!');
+    "
+    class="group p-3"
+  >
+    <div
+      class="h-4 w-4 fill-primary opacity-50 group-hover:opacity-70 dark:fill-white"
+    >
+      <slot />
     </div>
-  </a>
+  </component>
 </template>
