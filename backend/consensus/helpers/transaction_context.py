c1a1442664d6e2464e8a790d9228dad362359a63
henry-akwerigbe <henadek.ha@gmail.com>
2025-03-13 11:57:17 +0100

    refactor: removed unnecessary comment in TransactionContext

--- backend/consensus/helpers/transaction_context.py
+++ backend/consensus/helpers/transaction_context.py
@@ -18,27 +18,6 @@ from backend.protocol_rpc.message_handler.base import MessageHandler
 
 
 class TransactionContext:
-    """
-    Class representing the context of a transaction.
-
-    Attributes:
-        transaction (Transaction): The transaction.
-        transactions_processor (TransactionsProcessor): Instance responsible for handling transaction operations within the database.
-        chain_snapshot (ChainSnapshot): Snapshot of the chain state.
-        accounts_manager (AccountsManager): Manager for accounts.
-        contract_snapshot_factory (Callable[[str], ContractSnapshot]): Factory function to create contract snapshots.
-        node_factory (Callable[[dict, ExecutionMode, ContractSnapshot, Receipt | None, MessageHandler, Callable[[str], ContractSnapshot]], Node]): Factory function to create nodes.
-        msg_handler (MessageHandler): Handler for messaging.
-        consensus_data (ConsensusData): Data related to the consensus process.
-        iterator_rotation (Iterator[list] | None): Iterator for rotating validators.
-        remaining_validators (list): List of remaining validators.
-        num_validators (int): Number of validators.
-        contract_snapshot (ContractSnapshot | None): Snapshot of the contract state.
-        votes (dict): Dictionary of votes.
-        validator_nodes (list): List of validator nodes.
-        validation_results (list): List of validation results.
-    """
-
     def __init__(
         self,
         transaction: Transaction,
