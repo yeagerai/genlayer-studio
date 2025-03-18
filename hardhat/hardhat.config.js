062a5b05cc93c4a2bcde9b51bacb6db8c597e5da
Miguel Paya <miguelpaya3.5@gmail.com>
2025-03-19 00:14:40 +0100

    feat: almost finished

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -20,7 +20,7 @@ module.exports = {
   networks: {
     gethLocal: {
       url: "http://localhost:8545",
-      chainId: 31337,
+      chainId: 61999,
       accounts: [
         "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
         "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
