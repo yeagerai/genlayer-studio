90b287702888b176a2a4c705ef7699ac09663117
henry-akwerigbe <henadek.ha@gmail.com>
2025-03-25 15:22:15 +0100

    restored docstring for get_extra_validators

--- backend/consensus/algorithm/validator_management.py
+++ backend/consensus/algorithm/validator_management.py
@@ -118,13 +118,51 @@ class ValidatorManagement:
         consensus_data: ConsensusData,
         appeal_failed: int,
     ):
-        """Get extra validators for the appeal process."""
+        """
+        Get extra validators for the appeal process according to the following formula:
+        - when appeal_failed = 0, add n + 2 validators
+        - when appeal_failed > 0, add (2 * appeal_failed * n + 1) + 2 validators
+        Note that for appeal_failed > 0, the returned set contains the old validators
+        from the previous appeal round and new validators.
+
+        Selection of the extra validators:
+        appeal_failed | PendingState | Reused validators | Extra selected     | Total
+                      | validators   | from the previous | validators for the | validators
+                      |              | appeal round      | appeal             |
+        ----------------------------------------------------------------------------------
+               0      |       n      |          0        |        n+2         |    2n+2
+               1      |       n      |        n+2        |        n+1         |    3n+3
+               2      |       n      |       2n+3        |         2n         |    5n+3
+               3      |       n      |       4n+3        |         2n         |    7n+3
+                              └───────┬──────┘  └─────────┬────────┘
+                                      │                   |
+        Validators after the ◄────────┘                   └──► Validators during the appeal
+        appeal. This equals                                    for appeal_failed > 0
+        the Total validators                                   = (2*appeal_failed*n+1)+2
+        of the row above,                                      This is the formula from
+        and are in consensus_data.                             above and it is what is
+        For appeal_failed > 0                                  returned by this function
+        = (2*appeal_failed-1)*n+3
+        This is used to calculate n
+
+        Args:
+            all_validators (List[dict]): List of all validators.
+            consensus_history (dict): Dictionary of consensus rounds results and status changes.
+            consensus_data (ConsensusData): Data related to the consensus process.
+            appeal_failed (int): Number of times the appeal has failed.
+
+        Returns:
+            list: List of current validators.
+            list: List of extra validators.
+        """
+        # Get current validators and a dictionary mapping addresses to validators not used in the consensus process
         current_validators, validator_map = (
             ValidatorManagement.get_validators_from_consensus_data(
                 all_validators, consensus_data, False
             )
         )
 
+        # Remove used leaders from validator_map
         used_leader_addresses = (
             ValidatorManagement.get_used_leader_addresses_from_consensus_history(
                 consensus_history
@@ -134,6 +172,7 @@ class ValidatorManagement:
             if used_leader_address in validator_map:
                 validator_map.pop(used_leader_address)
 
+        # Set not_used_validators to the remaining validators in validator_map
         not_used_validators = list(validator_map.values())
 
         if len(not_used_validators) == 0:
@@ -141,16 +180,19 @@ class ValidatorManagement:
 
         nb_current_validators = len(current_validators) + 1
         if appeal_failed == 0:
+            # Calculate extra validators when no appeal has failed
             extra_validators = get_validators_for_transaction(
                 not_used_validators, nb_current_validators + 2
             )
         elif appeal_failed == 1:
+            # Calculate extra validators when one appeal has failed
             n = (nb_current_validators - 2) // 2
             extra_validators = get_validators_for_transaction(
                 not_used_validators, n + 1
             )
             extra_validators = current_validators[n - 1 :] + extra_validators
         else:
+            # Calculate extra validators when more than one appeal has failed
             n = (nb_current_validators - 3) // (2 * appeal_failed - 1)
             extra_validators = get_validators_for_transaction(
                 not_used_validators, 2 * n
