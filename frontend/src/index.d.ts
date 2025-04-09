3bdc6d8bbd3d195b3d7d36c5c806ada2f3450c6d
wizard <112275929+famouswizard@users.noreply.github.com>
2025-04-09 11:36:30 +0300

    chore: fix type for `value` prop in `vue3-json-viewer` declaration

--- frontend/src/index.d.ts
+++ frontend/src/index.d.ts
@@ -7,8 +7,9 @@ declare module 'vue3-json-viewer' {
     ComponentCustomProps,
     VNodeProps,
   } from 'vue';
+
   interface JsonViewerProps {
-    value: Object | Array<any> | string | number | boolean;
+    value: Record<string, unknown> | Array<any> | string | number | boolean;
     expanded: boolean;
     expandDepth: number;
     copyable: boolean | object;
@@ -18,12 +19,16 @@ declare module 'vue3-json-viewer' {
     previewMode: boolean;
     timeformat: (value: any) => string;
   }
+
   type JsonViewerType = JsonViewerProps &
     VNodeProps &
     AllowedComponentProps &
     ComponentCustomProps;
+
   const JsonViewer: Component<JsonViewerType>;
+
   export { JsonViewer };
+
   const def: { install: (app: App) => void };
   export default def;
 }
